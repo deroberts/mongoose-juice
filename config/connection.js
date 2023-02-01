8d38f3b060bb9be2945693fa3b3c3877bf85c3d5
deroberts <s501656@gmail.com>
2023-02-01 15:57:59 -0600

    added a few things, and added moment.js rather than my messing with my own date formatting.

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose');
+const { connect, connection } = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:127.0.0.1:27017/social-network-api', 
-{
+const connectionString = 
+process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api';
+
+connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}
-);
+});
 
-mongoose.set('debug', true);
+
 
 module.exports = connection;
